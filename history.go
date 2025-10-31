676bb398e64e78976ead9217128f0d37130a533c
taichi765 <taichi0209.youtub@gmail.com>
2025-10-31 09:57:44 +0900

    first commit

---
+++ history.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+type historyEntry struct {
+	time time.Time
+	kind personKind
+}
+
+type history struct {
+	entries []historyEntry
+	index   int // 現在の参照位置
+}
+
+func (h *history) Add(e historyEntry) {
+	h.entries = append(h.entries, e)
+	h.index = len(h.entries) // 最新に移動
+}
+
+func (h *history) Prev() *historyEntry {
+	if h.index > 0 {
+		h.index--
+	}
+	return &h.entries[h.index]
+}
+
+func (h *history) Next() *historyEntry {
+	if h.index < len(h.entries)-1 {
+		h.index++
+		return &h.entries[h.index]
+	}
+	h.index = len(h.entries) // 最後に戻る
+	return nil
+}
+func (h *history) Clear() {
+	h.entries = []historyEntry{}
+	h.index = 0
+}
+
+func (h *history) Pop() historyEntry {
+	last := h.entries[len(h.entries)-1]
+	h.entries = h.entries[:len(h.entries)-1]
+	return last
+}
+
+func (h *history) Save() (string, error) {
+	fileName := getFileName()
+	oldText, err := readOldText(fileName)
+	if err != nil {
+		return "", fmt.Errorf("[save] failed to read existing text: %w ", err)
+	}
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return "", fmt.Errorf("[save] failed to create file: %w", err)
+	}
+	w := bufio.NewWriter(f)
+
+	_, err = w.Write(oldText)
+	if err != nil {
+		return "", fmt.Errorf("[save] failed to write to buffer: %w", err)
+	}
+
+	for _, v := range h.entries {
+		line := fmt.Sprintf("%v,%v\n", v.time.String(), v.kind.String())
+		_, err := w.WriteString(line)
+		if err != nil {
+			return "", fmt.Errorf("[save] failed to write to buffer: %w", err)
+		}
+	}
+
+	err = w.Flush()
+	if err != nil {
+		return "", fmt.Errorf("[save] failed to flush buffer: %w", err)
+	}
+
+	return "Successfully saved.", nil
+}
+
+func readOldText(path string) ([]byte, error) {
+	f, err := os.Open(path)
+	if errors.Is(err, os.ErrNotExist) {
+		return []byte{}, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("[readOldText] failed to open file: %w", err)
+	}
+
+	oldText, err := io.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("[readOldText] failed to read all text from %v: %w", path, err)
+	}
+
+	return oldText, nil
+}
+
+func getFileName() string {
+	now := time.Now().Local().Day()
+
+	switch now {
+	case 30:
+		return "log_test_251030.csv"
+	case 31:
+		return "log_251031.csv"
+	case 1:
+		return "log_251101.csv"
+	case 2:
+		return "log_251102.csv"
+	default:
+		log.Fatal("文化祭は終了しました")
+		return ""
+	}
+}
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type personKind int
+
+const (
+	elemStudent personKind = iota
+	hsBoy
+	hsGirl
+	parent
+	other
+)
+
+func (k personKind) String() string {
+	switch k {
+	case elemStudent:
+		return "小学生"
+	case hsBoy:
+		return "中高生男子"
+	case hsGirl:
+		return "中高生女子"
+	case parent:
+		return "親"
+	case other:
+		return "その他"
+	default:
+		return "Unknown Kind"
+	}
+}
+
+type model struct {
+	elemStudentCount int
+	hsBoyCount       int
+	hsGirlCount      int
+	parentCount      int
+	otherCount       int
+
+	history history
+	message string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			msg, err := m.history.Save()
+			if err != nil {
+				log.Fatal(err)
+			}
+			m.message = msg
+			fmt.Println(msg)
+			return m, tea.Quit
+		case "backspace":
+			last := m.history.Pop()
+			switch last.kind {
+
+			case elemStudent:
+				m.elemStudentCount--
+			case hsBoy:
+				m.hsBoyCount--
+			case hsGirl:
+				m.hsGirlCount--
+			case parent:
+				m.parentCount--
+			case other:
+				m.otherCount--
+			default:
+				log.Fatal("[Update] unknown kind")
+			}
+		case "ctrl+s":
+			msg, err := m.history.Save()
+			if err != nil {
+				log.Fatal(err)
+			}
+			m.message = msg
+		case "g":
+			m.elemStudentCount++
+			m.history.Add(historyEntry{time: time.Now(), kind: elemStudent})
+			m.message = ""
+		case "h":
+			m.hsBoyCount++
+			m.history.Add(historyEntry{time: time.Now(), kind: hsBoy})
+			m.message = ""
+		case "j":
+			m.hsGirlCount++
+			m.history.Add(historyEntry{time: time.Now(), kind: hsGirl})
+			m.message = ""
+		case "k":
+			m.parentCount++
+			m.history.Add(historyEntry{time: time.Now(), kind: parent})
+			m.message = ""
+		case "l":
+			m.otherCount++
+			m.history.Add(historyEntry{time: time.Now(), kind: other})
+			m.message = ""
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	table := fmt.Sprintf(
+		"小学生: %d\n中高生男子: %d\n中高生女子: %d\n親: %d\nその他: %d\n\n",
+		m.elemStudentCount, m.hsBoyCount, m.hsGirlCount, m.parentCount, m.otherCount,
+	)
+	usage := "[g] 小学生+1, [h] 中高生男子+1, [j] 中高生女子, [k] 親, [l] その他, [q] quit\n"
+	return table + usage + m.message
+}
+
+func main() {
+	p := tea.NewProgram(model{})
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error:", err)
+	}
+}
