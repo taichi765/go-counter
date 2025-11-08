1b784117b79dcb630406dcca019d1524418553c9
taichi765 <taichi0209.youtub@gmail.com>
2025-11-08 22:47:09 +0900

    円グラフを見やすく

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import japanize_matplotlib
+from pathlib import Path
+from pandas import Series
 
 
 def init_bar_graph(file_path: str, graph_title: str):
@@ -22,11 +24,39 @@ def init_bar_graph(file_path: str, graph_title: str):
     ax.set_ylabel("Count")
 
 
-def init_pie_chart(file_path: str, graph_title: str):
-    df = pd.read_csv(file_path, header=None, names=["time", "kind"])
+def make_autopct(values: Series[int]):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct * total / 100.0))
+        return f"{pct:.1f}%\n({val})"
+
+    return my_autopct
+
+
+def init_pie_chart(file_path: str | None, graph_title: str):
+    """`file_path`が`None`のときは`logs/`の下のファイルを全部見る"""
+    if file_path is None:
+        folder = Path("../logs/")
+        df = pd.concat(
+            [
+                pd.read_csv(file, header=None, names=["time", "kind"])
+                for file in folder.glob("*.csv")
+            ],
+            ignore_index=True,
+        )
+    else:
+        df = pd.read_csv(file_path, header=None, names=["time", "kind"])
+
     counts = df["kind"].value_counts()
     _, ax = plt.subplots()
-    result = ax.pie(counts, startangle=90, counterclock=False)
+
+    result = ax.pie(
+        counts,
+        startangle=90,
+        counterclock=False,
+        autopct=make_autopct(counts),
+        pctdistance=0.8,
+    )
     ax.set_title(graph_title)
     ax.legend(
         result[0],
@@ -37,12 +67,13 @@ def init_pie_chart(file_path: str, graph_title: str):
     )
 
 
-init_bar_graph("../logs/log_251031.csv", "30分ごとの種類別来場者数 10/31")
+"""init_bar_graph("../logs/log_251031.csv", "30分ごとの種類別来場者数 10/31")
 init_bar_graph("../logs/log_251101.csv", "30分ごとの種類別来場者数 11/1")
 init_bar_graph("../logs/log_251102.csv", "30分ごとの種類別来場者数 11/2")
 
 init_pie_chart("../logs/log_251031.csv", "来場者の割合 10/31")
 init_pie_chart("../logs/log_251101.csv", "来場者の割合 11/1")
-init_pie_chart("../logs/log_251102.csv", "来場者の割合 11/2")
+init_pie_chart("../logs/log_251102.csv", "来場者の割合 11/2")"""
+init_pie_chart(None, "来場者の割合 合計")
 
 plt.show()
