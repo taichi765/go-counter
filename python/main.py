349f7ea3371fe5423e6c6ac8177b79dbce66d7c3
taichi765 <taichi0209.youtub@gmail.com>
2025-11-08 11:14:44 +0900

    円グラフも追加

--- python/main.py
+++ python/main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 
 
-def init_figure(file_path: str, graph_title: str):
+def init_bar_graph(file_path: str, graph_title: str):
     df = pd.read_csv(file_path, header=None, names=["time", "kind"])
     df["time"] = df["time"].map(lambda s: s.split(" +0900")[0])
     df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%d %H:%M:%S.%f")
@@ -22,7 +22,27 @@ def init_figure(file_path: str, graph_title: str):
     ax.set_ylabel("Count")
 
 
-init_figure("../logs/log_251031.csv", "30分ごとの種類別来場者数 10/31")
-init_figure("../logs/log_251101.csv", "30分ごとの種類別来場者数 11/1")
-init_figure("../logs/log_251102.csv", "30分ごとの種類別来場者数 11/2")
+def init_pie_chart(file_path: str, graph_title: str):
+    df = pd.read_csv(file_path, header=None, names=["time", "kind"])
+    counts = df["kind"].value_counts()
+    _, ax = plt.subplots()
+    result = ax.pie(counts, startangle=90, counterclock=False)
+    ax.set_title(graph_title)
+    ax.legend(
+        result[0],
+        list(counts.index),
+        title="Kinds",
+        loc="center left",
+        bbox_to_anchor=(1, 0, 0.5, 1),
+    )
+
+
+init_bar_graph("../logs/log_251031.csv", "30分ごとの種類別来場者数 10/31")
+init_bar_graph("../logs/log_251101.csv", "30分ごとの種類別来場者数 11/1")
+init_bar_graph("../logs/log_251102.csv", "30分ごとの種類別来場者数 11/2")
+
+init_pie_chart("../logs/log_251031.csv", "来場者の割合 10/31")
+init_pie_chart("../logs/log_251101.csv", "来場者の割合 11/1")
+init_pie_chart("../logs/log_251102.csv", "来場者の割合 11/2")
+
 plt.show()
